e01ccb2d1e9dc769474c7dc7a378768c310bd335
SerenaDonadi <serena.donadi@slu.se>
2025-08-21 14:34:11 +0200

    read data

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# HIGH5
-Aim: to develop an index, which can substitute VIX, to assess the status of rivers (small and large) focusing on trout, both resident and migratory.
+
+HIGH FIVE
+
+Aim: to develop an index, which can substitute VIX, to assess the status of rivers (small and large) focusing on trout, both resident and migratory. A model that can predict the max/optimal density of trout (0+) based on habitat variables, including THS. Possibly with indication of uncertainty.
+
+Methods: use the approach of Katarina M., i.e. find reference points for each site using time series as well as spatial extended data. But this time include also resident trout. Ask Katarina for the script. Develop a model to predict the reference points based on habitat variables, including THS.
+Focus on trout 0+
+Start with, hence collected the data for, one county: Jämtland. Joacim will give me the data, after filtering out poor quality data or sits to exclude.
+Norway is involved also (Nina and others), they haven’t provided the data yet. Meeting in September where we should show something
+
+I received the data from Joacim: all location, only Jämtland and Jämtland filetred according to the criteria listed in the document “filtering”. Keep a variable saying if the whole width has been sampled and use it as predictor. Uso all years, also the old ones. If data are not enough, we can possibly add also Dalarna data.
+
+# questions 
+
+check the meaning of different VIX variables
---
+++ jamtland_analysis.R
@@ -0,0 +1,124 @@
+rm(list=ls())
+
+dir.exists("//storage-dh.slu.se/home$/sedi0002/My Documents/Job/HIGH5")
+setwd("G:/My Drive/HIGH5/HIGH5_data")
+
+
+# Libraries ---------------------------------------------------------------
+
+#library(tidyverse)
+library(ggplot2)
+library(dplyr)
+#library(tidyr)
+library(gplots)
+library(lattice)
+library(nlme)
+# library(MASS) # potenital name clash problem for function select in dplyr
+library(piecewiseSEM)
+library(lme4)
+library(car)
+library(visreg)
+library(mgcv)
+library(ggeffects)
+
+library(ExcelFunctionsR)
+#library(plyr)
+
+
+#####
+# Read Datasets
+#####
+
+# if ANSI doesn't work, try: encoding = "UTF-8", or encoding ="ISO-8859-1", or "latin1"
+
+# to calculate the probability of a file of being encoded in several encodings
+library(readr)
+guess_encoding("SERS_20250522_Jamtland_Filtered.csv", n_max = 1000)
+# try both encoding = "" and fileEncoding = ""
+
+jamtland <- read.csv2("SERS_20250522_Jamtland_Filtered.csv",fileEncoding="ISO-8859-1", header=TRUE, sep=";", dec=",") 
+head(jamtland)
+
+# check response variable:
+summary(jamtland$Öring0)
+
+# check values "-9":
+jamtland %>% 
+  filter(Öring0 == -9) 
+# 2 records. maybe typos. Exlude them:
+jamtland2 <- jamtland %>% 
+  filter(Öring0 != -9)
+
+# check spatial hierarchy:
+unique(jamtland2$Län)
+unique(jamtland2$Länskod)
+unique(jamtland2$Haro_nr)
+unique(jamtland2$Huvudavrinningsområde) #6 levels
+unique(jamtland2$Hflodomr) #6
+unique(jamtland2$Biflnr)
+unique(jamtland2$Vattendrag ) #476
+unique(jamtland2$Lokal)#687
+unique(jamtland2$Lokalnr)#67
+
+unique(jamtland2$Syfte)
+unique(jamtland2$Vandhind)
+unique(jamtland2$Typavpop)
+unique(jamtland2$Lokalvar)
+unique(jamtland2$Lokalvar)
+
+table(jamtland2$VTYP_ED)
+table(jamtland2$Typavpop)
+
+
+head(jamtland)
+# retain only variables of interest:
+jamtland3 <- jamtland2 %>% 
+  select(Öring0, 
+         Hflodomr, 
+         Vattendrag, 
+         Lokal, 
+         Langd, 
+         XKOORLOK, 
+         YKOORLOK,
+         WGS84_Dec_N,
+         WGS84_Dec_E,
+         Fiskedatum,
+         ÅR,
+         MÅNAD,
+         Bredd,
+         Maxdjup,
+         Medeldju,
+         Substr1,
+         Vattente,
+         Beskuggn,
+         Vandhind,
+         VTYP_ED,
+         Typavpop,
+         Hoh,
+         Avstupp,
+         Avstner,
+         mindistsj,
+         LUTNING_PROM,
+         MEDTEMPAR,
+         MEDT_JULI,
+         VIX, 
+         VIX_klass)
+
+head(jamtland3)
+
+# explore hierarchical spatial and temporal structure.
+# how many years per site? first and last year of sampling?
+site_years<-jamtland3 %>% 
+  group_by(Hflodomr,Vattendrag,Lokal) %>% 
+  summarise(n_dinstic_years = n_distinct(ÅR),
+            first_year = min(ÅR),
+            last_year = max(ÅR)) %>% 
+  arrange(desc(n_dinstic_years))
+
+# select only sites with at least 10 years of sampling 
+site_years10<-site_years %>%
+  filter(n_dinstic_years > 9)
+
+# select a subset in jamtland3 whose sites match those in the site_years10:
+jamtland3_time_series<-jamtland3 %>%
+  filter(Lokal %in% site_years10$Lokal)
